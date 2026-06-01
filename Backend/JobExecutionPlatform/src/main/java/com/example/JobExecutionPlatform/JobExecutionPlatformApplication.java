a8dc654063531b3b12463c33031bb1e5d69aa22d
shivam gupta <shivamgupta30121998@gmail.com>
2026-06-01 22:34:07 +0530

    :tada: Project initialization

---
+++ Backend/JobExecutionPlatform/src/main/java/com/example/JobExecutionPlatform/JobExecutionPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.JobExecutionPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobExecutionPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobExecutionPlatformApplication.class, args);
+    }
+
+}
